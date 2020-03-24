3d2f3603ea4793915e46f061d20f3b0021dffdf6
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-24 17:31:29 -0500

    added main container

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 const MainContent = document.getElementById('content');
 const PageContent = document.createElement('div');
 PageContent.classList.add('page-content');
+PageContent.classList.add('container');
 
 export { MainContent, PageContent };
