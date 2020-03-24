c9a7db28b412224834c54bee19f5a855e8d8d2f8
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-24 10:26:41 -0500

    created Content component

---
+++ src/components/Content.js
@@ -0,0 +1,5 @@
+const MainContent = document.getElementById('content');
+const PageContent = document.createElement('div');
+PageContent.classList.add('page-content');
+
+export { MainContent, PageContent };
