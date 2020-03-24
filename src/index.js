b8f387fef698beab2a5b7338443ac2eb28ae6832
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-24 10:33:09 -0500

    create the index.js to call the app behavior

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import currentHash from './utils/currentHash';
+import {
+  Header, Home, Menu, Contact,
+} from './components/Header';
+import { MainContent, PageContent } from './components/Content';
+import { appendContent, innerContent } from './utils/injectContent';
+import HomePage from './pages/home.html';
+import MenuPage from './pages/menu.html';
+import ContactPage from './pages/contact.html';
+
+
+window.addEventListener('load', e => {
+  appendContent(MainContent, Header);
+  appendContent(MainContent, PageContent);
+
+  switch (currentHash()) {
+    case '#home':
+      innerContent(PageContent, HomePage);
+      break;
+    case '#menu':
+      innerContent(PageContent, MenuPage);
+      break;
+    case '#contact':
+      innerContent(PageContent, ContactPage);
+      break;
+    default:
+      innerContent(PageContent, HomePage);
+  }
+});
+
+Home.addEventListener('click', () => { innerContent(PageContent, HomePage); });
+Menu.addEventListener('click', () => { innerContent(PageContent, MenuPage); });
+Contact.addEventListener('click', () => { innerContent(PageContent, ContactPage); });
